ac10b5305cd80664cdd381e768f7c345b6a8cdd4
Valentine Cheluchi <krixxvalle@gmail.com>
2025-07-20 22:42:02 +0100

    django view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,16 @@
 # relationship_app/views.py
-from django.http import HttpResponse
+
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    lines = [f"{book.title} by {book.author.name}" for book in books]
-    return HttpResponse("\n".join(lines))
+    # Renders the template at relationship_app/list_books.html
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view: detail of a library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
