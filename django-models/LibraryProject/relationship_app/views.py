5bc6cc461c0a641ead6bf6939490d60154999a0d
Valentine Cheluchi <krixxvalle@gmail.com>
2025-07-20 22:48:12 +0100

    django view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 # relationship_app/views.py
 
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
