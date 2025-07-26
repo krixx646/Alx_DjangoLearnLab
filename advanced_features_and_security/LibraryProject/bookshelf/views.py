cc29449767081befc5edd206c78315c9c191ab21
Valentine Cheluchi <krixxvalle@gmail.com>
2025-07-26 19:19:14 +0100

    django view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from django.http import HttpResponse
+from .forms import ExampleForm
 
 # Create your views here.
 
