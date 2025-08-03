559d891ce164fcc701044c9de3df2966845b2a52
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-03 13:39:52 +0100

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,4 +9,8 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet (viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 # Create your views here.
