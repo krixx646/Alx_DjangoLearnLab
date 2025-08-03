17c2fdb48cc429f99ce7264f6b42fd95dea0d62b
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-03 07:36:30 +0100

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 # Create your views here.
