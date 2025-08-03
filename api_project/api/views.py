68881db5391693534a9bdfad258081c52fba02fa
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-03 07:40:13 +0100

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,9 +3,9 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
