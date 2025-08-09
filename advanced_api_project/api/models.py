4448797a3b2ed60f9228c11fbab7ff2ebc3cfdf8
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-10 00:16:09 +0100

    api

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# In Django, a "model" describes the structure of the data in the database
+# (like a blueprint for a table — it says what fields exist and how they relate).
+# A "serializer" is used to convert model data into formats like JSON (for APIs),
+# and also to turn incoming data (like from a web form or request) back into a model.
+# Adding comments that explain what each specific model and serializer is for
+# makes it easier for anyone reading the code to quickly understand the purpose
+# of each one without having to guess or read through all the logic.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=225)
+
+class Book (models.Model):
+    title = models.CharField(max_length=225)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField()
+
---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,32 @@
+from .models import Author, Book
+from rest_framework import serializers
+from datetime import date
+
+# The relationship between Author and Book is handled using a nested serializer,
+# allowing BookSerializer to include detailed Author information (via AuthorSerializer)
+# rather than just an ID. This makes it possible to read and write data for both models
+# in a single request while preserving their foreign key relationship.
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+            current_year = date.today().year
+            if value > current_year:
+                raise serializers.ValidationError("Publication year cannot be in the future.")
+            return value
+
+
+    
+
+class AuthorSerializer(serializers.ModelSerializer):
+    book = BookSerializer(many=True, read_only=True)
+    class Meta:
+        model = Author
+        fields = ['name', 'book']
+
+
+
