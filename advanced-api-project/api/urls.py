a0c03d71cffbb9e5aac7dceaae48730f44f08875
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-10 04:32:19 +0100

    api

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     path('authors/', AuthorListView.as_view(), name='author-list'),
     path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
     path('authors/create/', AuthorCreateView.as_view(), name='author-create'),
-    path('authors/<int:pk>/update/', AuthorUpdateView.as_view(), name='author-update'),
-    path('authors/<int:pk>/delete/', AuthorDeleteView.as_view(), name='author-delete'),
+    path('authors/update/', AuthorUpdateView.as_view(), name='author-update'),
+    path('authors/delete/', AuthorDeleteView.as_view(), name='author-delete'),
     # Book URLs
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 
 ]
