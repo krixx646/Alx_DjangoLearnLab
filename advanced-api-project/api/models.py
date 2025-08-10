fecce69dc545bfd7ea2b05a177a9e6927c078e7d
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-10 01:59:45 +0100

    api

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -12,7 +12,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=225)
 
-class Book (models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=225)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     publication_year = models.IntegerField()
