2a28e9f035748b5079f09b907aec51fd7959924a
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-10 04:57:42 +0100

    api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,6 +7,7 @@ from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework import filters
 
 
 
