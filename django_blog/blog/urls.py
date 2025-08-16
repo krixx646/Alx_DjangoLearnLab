15864fb920ce4c54117819cf573c41090dee5d74
Valentine Cheluchi <krixxvalle@gmail.com>
2025-08-16 17:53:09 +0100

    django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
     
-    path('posts/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('posts/<int:post_pk>/comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('posts/<int:post_pk>/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('post/<int:pk>/comments/new/', views.comment_create, name='comment_create'),
+    path('comment/<int:pk>/update/', views.comment_update, name='comment_update'),
+    path('comment/<int:pk>/delete/', views.comment_delete, name='comment_delete'),
 ]
